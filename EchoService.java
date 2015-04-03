0656094435c5395f9af018d40261eeba03f6b992
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2015-04-03 11:05:59 +0100

    EchoService Interface

---
+++ EchoService.java
@@ -0,0 +1,25 @@
+package day22;
+
+import java.rmi.Remote;
+
+/*
+ * client -server architecture
+ * Create the interface
+ * it extends the interface Remote and all methods throw RemoteException (?)
+ * Implement the interface in a class - extends UniCast.. class
+ * Launch the server - Security manager (new RMISecurityManager())
+ * We generate a server object that is to be binded to a registry naming.rebind(String, Remote)
+ * 
+ */
+
+public interface EchoService extends Remote{
+	
+	/**
+	 * The method returns the same string that was passed as an argument
+	 * @param s
+	 * @return returns the same string that was passed as an argument
+	 */
+
+	public String echo (String s);
+	
+}
